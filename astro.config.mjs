e695a9f3098a3c24411b33264c461bd334a23591
Marc Heatley <marc@properdesign.co.uk>
2025-02-23 17:49:51 +0000

    added netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,21 @@ import icon from "astro-icon";
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 
+import netlify from "@astrojs/netlify";
+
 export default defineConfig({
-	integrations: [
-		icon(),
-		sanity({
-			projectId: "tsbiqnoe",
-			dataset: "production",
-			// Set useCdn to false if you're building statically.
-			useCdn: false,
-			studioBasePath: "/admin",
-		}),
-		,
-		react(),
+  integrations: [
+      icon(),
+      sanity({
+          projectId: "tsbiqnoe",
+          dataset: "production",
+          // Set useCdn to false if you're building statically.
+          useCdn: false,
+          studioBasePath: "/admin",
+      }),
+      ,
+      react(),
 	],
+
+  adapter: netlify(),
 });
